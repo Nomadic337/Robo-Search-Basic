03b12015c7154c8e2bc0db76a787cec753514ee1
Wolf S Laleau <devlaleau@gmail.com>
2020-06-13 11:38:30 -0400

    Can now take  values from API

--- src/App.js
+++ src/App.js
@@ -9,9 +9,15 @@ class App extends Component {
     constructor() {
         super()
         this.state = {          // describes what can change in our App
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({ robots: robots}));
     }
 
     onSearchChange = (event) => {
@@ -22,14 +28,18 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
-        return (
-            <div className='tc'>
-              <h1 className='f1'>RoboFriends</h1>
-              <SearchBox searchChange={this.onSearchChange} />
-              <CardList robots={filteredRobots} />
-            </div>
-      
-          );
+        if(this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={filteredRobots} />
+                </div>
+        
+            );
+        }
 
     }
 
