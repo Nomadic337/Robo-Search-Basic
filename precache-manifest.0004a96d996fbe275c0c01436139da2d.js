69ab55a39273c46ac648e1002f12f9134c9fa6a4
Wolf S Laleau <devlaleau@gmail.com>
2020-06-13 19:21:08 -0400

    Updates

---
+++ precache-manifest.0004a96d996fbe275c0c01436139da2d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03751aa4b77aeb8830fb8ccf2c452e3a",
+    "url": "/Robo-Search-Basic/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/Robo-Search-Basic/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1005162f0ef4e7d4fd30",
+    "url": "/Robo-Search-Basic/static/css/main.82af6cd4.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/Robo-Search-Basic/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robo-Search-Basic/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1005162f0ef4e7d4fd30",
+    "url": "/Robo-Search-Basic/static/js/main.6404430d.chunk.js"
+  },
+  {
+    "revision": "8be0b0ca9a2cd51c026f",
+    "url": "/Robo-Search-Basic/static/js/runtime-main.ba80b363.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robo-Search-Basic/static/media/SEGA.12a3db8b.woff"
+  }
+]);
