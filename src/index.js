e9cbc4cd4f5007e48f56596232423a416567e8de
Wolf S Laleau <devlaleau@gmail.com>
2020-06-12 20:13:29 -0400

    Input detection functionality is working

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
-ReactDOM.render(<h1>Hello World!</h1>
+ReactDOM.render(<App />
 , document.getElementById('root')
 );
 
